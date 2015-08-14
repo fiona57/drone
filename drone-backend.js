05b5defbc8cc3d8dec07fb9a0828ef556c2b305d
V5782 (Course ID) <v5782@cam.ac.uk>
2015-08-14 14:33:25 +0100

    probably doesn't work

--- drone-backend.js
+++ drone-backend.js
@@ -11,11 +11,30 @@ Cylon.robot()
         driver: "ardrone",
         connection: "ardrone"
     })
-    .on("ready", fly);
+    .on("ready", fly)
+    .device("nav",{
+        driver: "ardrone-nav",
+        connection: "ardrone"
+    })
+;
+bot.nav.on("navdata", function(data){
+    console.log(data);
+}
     
-// Fly the bot
+// Fly the bot - modify this to give instructions for flight path
 function fly(robot) {
+    bot=robot;
+    bot.drone.disableEmergency();
+    bot.drone.ftrim();
+    bot.drone.takeoff();
+    after (10*1000, function(){
 
+    bot.drone.land();
+});
+    after(15*1000, function(){
+        bot.drone.stop();
+
+    });
 }
 
 Cylon.start();
